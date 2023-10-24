956c551761eaf2a2b3ae951da0336827f506428d
PC <burakugras67@gmail.com>
2023-10-24 17:41:43 +0300

    Çok boyutlu diziler

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,10 +6,29 @@
 
 
         string[] students2 = { "Engin", "Derin", "Salih" };
+        //students2[3] = "Ahmet";
 
-        foreach (string student in students2)
+        //foreach (string student in students2)
+        //{
+        //    Console.WriteLine(student);
+        //}
+
+        string[,] regions = new string[5, 3]
         {
-            Console.WriteLine(student);
+            {"İstanbul","İzmit","Balıkesir" },
+            {"Ankara","Konya","Kırıkkale" },
+            {"Antalya","Adana","Mersin" },
+            {"Rize","Trabzon","Samsun"},
+            {"İzmir","Muğla","Manisa" }
+        };
+
+        for (int i = 0; i <= regions.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= regions.GetUpperBound(1); j++)
+            {
+                Console.WriteLine(regions[i,j]);
+            }
+            Console.WriteLine("**********");
         }
 
         Console.ReadLine();
