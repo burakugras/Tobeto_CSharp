0691c6e207a9a7360f846d2a42919f57e340286f
PC <burakugras67@gmail.com>
2023-10-24 16:20:34 +0300

    Var keyword ile çalışmak

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,14 +12,18 @@
         short number3 = 32767;//16 bit
         int number1 = 2147483647;//32 bit
         long number2 = 9223372036854775807;//64 bit
+        var number7 = 10;
+        //number7 = "A";
 
         Console.WriteLine("Number1 is {0}",number1);
         Console.WriteLine("Number2 is {0}",number2);
         Console.WriteLine("Number3 is {0}",number3);
         Console.WriteLine("Number4 is {0}",number4);
         Console.WriteLine("Number5 is {0}",number5);
+        Console.WriteLine("Number7 is {0}",number7);
         Console.WriteLine("Character is {0}",(int)character);
         Console.WriteLine((int)Days.Friday);
+
         Console.ReadLine();
     }
 }
