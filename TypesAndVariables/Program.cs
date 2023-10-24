904303d5d65acb589dfce0e6ff25c50fc727d80f
PC <burakugras67@gmail.com>
2023-10-24 16:18:18 +0300

    Enum veri tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         //value types
-
+        
         double number5 = 10.4;//64 bit
         decimal number6 = 10.4M;
         char character = 'A';//16 bit
@@ -12,12 +12,19 @@
         short number3 = 32767;//16 bit
         int number1 = 2147483647;//32 bit
         long number2 = 9223372036854775807;//64 bit
+
         Console.WriteLine("Number1 is {0}",number1);
         Console.WriteLine("Number2 is {0}",number2);
         Console.WriteLine("Number3 is {0}",number3);
         Console.WriteLine("Number4 is {0}",number4);
         Console.WriteLine("Number5 is {0}",number5);
         Console.WriteLine("Character is {0}",(int)character);
+        Console.WriteLine((int)Days.Friday);
         Console.ReadLine();
     }
+}
+
+enum Days
+{
+    Monday=1,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
 }
