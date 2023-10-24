cdf392298655c49b6e0ee722c4ad7b890e9cb173
PC <burakugras67@gmail.com>
2023-10-24 16:01:43 +0300

    Long veri tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -5,7 +5,9 @@
         //value types
 
         int number1 = 2147483647;
+        long number2 = 9223372036854775807;
         Console.WriteLine("Number1 is {0}",number1);
+        Console.WriteLine("Number1 is {0}",number2);
         Console.ReadLine();
     }
 }
