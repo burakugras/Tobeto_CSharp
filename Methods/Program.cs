a3bf02952d7d8b3a39a2ee2076b31c29e0d04a3c
PC <burakugras67@gmail.com>
2023-10-24 17:25:36 +0300

    Params keyword ile çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -22,6 +22,8 @@
         Console.WriteLine(Multiply(2,4));
         Console.WriteLine(Multiply(2,4,5));
 
+        Console.WriteLine(Add4(1,2,3,4,5,6));
+
         Console.ReadLine();
     }
 
@@ -49,5 +51,10 @@
     static int Multiply(int number1, int number2,int number3)
     {
         return number1 * number2*number3;
+    }
+
+    static int Add4(int number,params int[] numbers)
+    {
+        return numbers.Sum();
     }
 }
