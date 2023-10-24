8ed3546e0a27505c61e8e268cacb0032f330ea78
PC <burakugras67@gmail.com>
2023-10-24 16:32:32 +0300

    Switch bloğuyla çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -4,17 +4,30 @@
     {
         var number = 11;
 
-        if (number == 10)
+        //if (number == 10)
+        //{
+        //    Console.WriteLine("Number is 10");
+        //}
+        //else if(number==20)
+        //{
+        //    Console.WriteLine("Number is 20");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Number is not 10 or 20");
+        //}
+
+        switch (number)
         {
-            Console.WriteLine("Number is 10");
-        }
-        else if(number==20)
-        {
-            Console.WriteLine("Number is 20");
-        }
-        else
-        {
-            Console.WriteLine("Number is not 10 or 20");
+            case 10:
+                Console.WriteLine("Number is 10");
+                break;
+            case 20:
+                Console.WriteLine("Number is 20");
+                break;
+            default:
+                Console.WriteLine("Number is not 10 or 20");
+                break;
         }
 
         Console.ReadLine();
