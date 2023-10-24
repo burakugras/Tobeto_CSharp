57bc91bdb4c7dd9c7519b435aa11e924af65fab6
PC <burakugras67@gmail.com>
2023-10-24 18:11:18 +0300

    String ile çalışmak

---
+++ Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //stringler aslında bir char array'dir.
+        string city = "Ankara";
+        //Console.WriteLine(city[0]);
+
+        foreach (var item in city)
+        {
+            Console.WriteLine(item);
+        }
+
+        string city2 = "İstanbul";        
+
+        Console.WriteLine(String.Format("{0} {1}",city,city2));
+        Console.ReadLine();
+    }
+}
