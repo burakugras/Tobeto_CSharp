05259ff174a1a65c4fa08fb45f77c7692252ac92
PC <burakugras67@gmail.com>
2023-10-24 16:04:20 +0300

    Short veri tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,10 +4,12 @@
     {
         //value types
 
-        int number1 = 2147483647;
-        long number2 = 9223372036854775807;
+        int number1 = 2147483647;//32 bit
+        long number2 = 9223372036854775807;//64 bit
+        short number3 = 32767;//16 bit
         Console.WriteLine("Number1 is {0}",number1);
         Console.WriteLine("Number1 is {0}",number2);
+        Console.WriteLine("Number1 is {0}",number3);
         Console.ReadLine();
     }
 }
