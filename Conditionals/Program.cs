632d3d5146f0a99e48b1f4ca073227d6172dbf31
PC <burakugras67@gmail.com>
2023-10-24 16:39:15 +0300

    Demo: Çoklu şartlarla çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        var number = 11;
+        var number = 201;
 
         //if (number == 10)
         //{
@@ -17,17 +17,30 @@
         //    Console.WriteLine("Number is not 10 or 20");
         //}
 
-        switch (number)
+        //switch (number)
+        //{
+        //    case 10:
+        //        Console.WriteLine("Number is 10");
+        //        break;
+        //    case 20:
+        //        Console.WriteLine("Number is 20");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Number is not 10 or 20");
+        //        break;
+        //}
+
+        if (number >= 0 && number <= 100)
         {
-            case 10:
-                Console.WriteLine("Number is 10");
-                break;
-            case 20:
-                Console.WriteLine("Number is 20");
-                break;
-            default:
-                Console.WriteLine("Number is not 10 or 20");
-                break;
+            Console.WriteLine("Number is between 0-100");
+        }
+        else if(number >= 100 && number <=200)
+        {
+            Console.WriteLine("Number is between 101-200");
+        }
+        else if (number > 200 || number <0)
+        {
+            Console.WriteLine("Number is less than 0 or greater than 200");
         }
 
         Console.ReadLine();
