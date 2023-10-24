993397c73bfef1974c9e80663f5c85710cc4091f
PC <burakugras67@gmail.com>
2023-10-24 17:07:15 +0300

    Ref keyword ile çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,7 +8,15 @@
         Add();
 
         var result = Add2(4);
-        Console.WriteLine(result);
+
+        int number1 = 20;
+        int number2 = 100;
+
+        var result2=Add3(ref number1, number2);
+
+
+        Console.WriteLine(result2);
+        Console.WriteLine(number1);
 
         Console.ReadLine();
     }
@@ -22,5 +30,11 @@
     {
         var result = number1 + number2;
         return result;
+    }
+
+    static int Add3(ref int number1,int number2)
+    {
+        number1 = 30;
+        return number1 + number2;
     }
 }
