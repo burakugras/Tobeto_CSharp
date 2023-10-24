41829d134ef50f243c69e07beb314088d70b0942
PC <burakugras67@gmail.com>
2023-10-24 16:06:12 +0300

    Byte veri tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,12 +4,14 @@
     {
         //value types
 
+        byte number4 = 255;
+        short number3 = 32767;//16 bit
         int number1 = 2147483647;//32 bit
         long number2 = 9223372036854775807;//64 bit
-        short number3 = 32767;//16 bit
         Console.WriteLine("Number1 is {0}",number1);
         Console.WriteLine("Number1 is {0}",number2);
         Console.WriteLine("Number1 is {0}",number3);
+        Console.WriteLine("Number1 is {0}",number4);
         Console.ReadLine();
     }
 }
