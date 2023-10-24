f8bfd0ca2847156a54051e897c3c4680a37a18f7
PC <burakugras67@gmail.com>
2023-10-24 17:33:23 +0300

    Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] students = new string[3] { "Engin", "Derin", "Salih" };
+
+
+        string[] students2 = { "Engin", "Derin", "Salih" };
+
+        foreach (string student in students2)
+        {
+            Console.WriteLine(student);
+        }
+
+        Console.ReadLine();
+    }
+}
